d62fe5c268dd500053e73e40717a4ba082794423
Alexandr Rodik <rodik@readdle.com>
2018-04-22 15:26:03 +0300

    add growl notification for dev command

--- cli/commands/dev.js
+++ cli/commands/dev.js
@@ -1,9 +1,25 @@
 const webpack = require("webpack"),
-    Utils = require("../utils");
+    Utils = require("../utils"),
+    notifier = require("node-notifier");
 
 module.exports = function(webpackConfig) {
     const compiler = webpack(webpackConfig);
-    const watching = compiler.watch({}, Utils.printBriefWebpackStats);
+    const watching = compiler.watch({}, function(error, stats) {
+        if (Utils.webpackStatsHasErrors(error, stats)) {
+            notifier.notify({
+                title: "FFBT",
+                message: "An error occurred during compilation!",
+            });
+        } else {
+            notifier.notify({
+                title: "FFBT",
+                message: "Compiled successfully",
+                timeout: 2
+            });
+        }
+
+        return Utils.printBriefWebpackStats(error, stats);
+    });
 
     ['SIGINT', 'SIGTERM'].forEach((sig) => {
         process.on(sig, () => {
