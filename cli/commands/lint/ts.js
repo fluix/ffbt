cab4b7ea4d0846f4b61b1b7b65f621b5e9eee9e4
Drew Mc'Intire <amatiushchenko@readdle.com>
2018-08-08 12:21:46 +0300

    fix pr comments

--- cli/commands/lint/ts.js
+++ cli/commands/lint/ts.js
@@ -7,9 +7,8 @@ module.exports = function (ffbt_root, workdir, argv, projectConfig, PROJECT_ROOT
     const force = (typeof argv.force === "undefined") ? false : Boolean(argv.force);
     let configPath = path.resolve(ffbt_root, "./linters/tslint.json");
 
-    if(projectConfig.tsLintConfigPath) {
+    if (projectConfig.tsLintConfigPath) {
         configPath = path.resolve(PROJECT_ROOT_PATH, projectConfig.tsLintConfigPath);
-        console.log("test", configPath);
     }
 
 
