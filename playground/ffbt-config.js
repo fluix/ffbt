32207849d13ee3365b062455b1e1d08ca71ca4e5
Drew <amatiushchenko@readdle.com>
2018-08-31 19:03:49 +0300

    init playground project

---
+++ playground/ffbt-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    profiles: {
+        dev: {
+        },
+        production: {
+        },
+    },
+    supportedBrowsers: [
+        "Android >= 4",
+        "Chrome >= 40",
+        "Firefox >= 40",
+        "Explorer >= 11",
+        "iOS >= 8",
+        "Safari >= 8.0"
+    ],
+    buildPath: "./public/",
+};
---
+++ playground/stylelint.config.js
@@ -0,0 +1,1 @@
+module.exports = require("ffbt/linters/stylelint.config");
