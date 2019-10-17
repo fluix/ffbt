f10ad0f67e28691773b7630c4cab92cf920b518f
Alexandr Rodik <rodik@readdle.com>
2019-10-17 11:43:03 +0300

    remove useless type

--- src/webpack/types.ts
+++ src/webpack/types.ts
@@ -1,8 +1,6 @@
 import {ProjectConfig} from "../project-config";
 import * as webpack from "webpack";
 import {ProjectPaths} from "../paths";
-import {DeepPartial} from "utility-types";
 
 export type WebpackLayerConfigurator = (projectConfig: ProjectConfig, paths: ProjectPaths) => webpack.Configuration;
-// export type WebpackLayerConfigurator = (projectConfig: ProjectConfig, paths: ProjectPaths) => DeepPartial<webpack.Configuration>;
 
