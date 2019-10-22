6aed4822f1f7231f7c348e8cef7358797ce348d6
Alexandr Rodik <rodik@readdle.com>
2019-10-22 15:26:03 +0300

    ability to run dev mode with watch and dev with web server

--- src/cli/commands/dev.ts
+++ src/cli/commands/dev.ts
@@ -1,9 +1,11 @@
-import {Command, flags} from "@oclif/command";
+import {flags} from "@oclif/command";
 import * as Parser from "@oclif/parser";
 import {BaseWebpackCommand} from "../base-webpack-command";
 import {ServiceRunStrategy} from "../../services/webpack/runner";
 import {RunWebpackDevServerStrategy} from "../../services/webpack/runner/run-webpack-dev-server";
 import * as webpack from "webpack";
+import {BaseCommand} from "../base-command";
+import {RunWebpackCompileWatcherStrategy} from "../../services/webpack/runner/run-compile-watcher";
 
 enum Arguments {
     workingDirectory = "working_directory",
@@ -16,10 +18,22 @@ export default class DevCommand extends BaseWebpackCommand {
         }
     ];
 
-    static flags = BaseWebpackCommand.flags;
+    static flags: flags.Input<any> = {
+        server: flags.boolean({
+            default: false,
+            description: "Run a Development Server"
+        }),
+        ...BaseCommand.flags,
+    };
 
     getWebpackRunner(webpackConfig: webpack.Configuration): ServiceRunStrategy {
-        return new RunWebpackDevServerStrategy(webpackConfig);
+        const {flags} = this.parse(DevCommand);
+
+        if (flags.server) {
+            return new RunWebpackDevServerStrategy(webpackConfig);
+        }
+
+        return new RunWebpackCompileWatcherStrategy(webpackConfig);
     }
 
     async run() {
--- src/services/webpack/runner/make-bundle.ts
+++ src/services/webpack/runner/make-bundle.ts
@@ -1,19 +1,13 @@
 import {ServiceRunStrategy} from "./index";
 import * as webpack from "webpack";
+import {printVerboseWebpackStats} from "../utils/output";
 
 export class MakeBundleStrategy implements ServiceRunStrategy {
     constructor(private webpackConfig: webpack.Configuration) {
     }
 
     run(): void {
-        // console.log("RUN BUILD", inspect(projectConfig, {showHidden: false, depth: null}));
         const compiler = webpack(this.webpackConfig);
-
-        compiler.run((error, stats) => {
-            // eslint-disable-next-line no-console
-            console.log(stats.toString({
-                colors: true,
-            }));
-        });
+        compiler.run(printVerboseWebpackStats);
     }
 }
