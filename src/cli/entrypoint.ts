9135e43ecfe5deec320cb5f7c20344aa607cfbdb
Alexandr Rodik <rodik@readdle.com>
2019-10-23 17:15:15 +0300

    add readme to the entrypoint for potential contributor

--- src/cli/entrypoint.ts
+++ src/cli/entrypoint.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+// This is the OClif entrypoint
+// Entrypoints for developers located in "commands" directory
+// If you're trying to figure out what's going on - go to there and look for the command you interesting in
+
 require('@oclif/command').run()
     .then(require('@oclif/command/flush'))
     .catch(require('@oclif/errors/handle'));
