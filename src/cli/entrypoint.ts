30b730b3dccc301c7582a2858a3427db5645aed6
Alexandr Rodik <rodik@readdle.com>
2019-10-23 17:15:43 +0300

    typo fix

--- src/cli/entrypoint.ts
+++ src/cli/entrypoint.ts
@@ -2,7 +2,7 @@
 
 // This is the OClif entrypoint
 // Entrypoints for developers located in "commands" directory
-// If you're trying to figure out what's going on - go to there and look for the command you interesting in
+// If you're trying to figure out what's going on - go there and look for the command you interesting in
 
 require('@oclif/command').run()
     .then(require('@oclif/command/flush'))
