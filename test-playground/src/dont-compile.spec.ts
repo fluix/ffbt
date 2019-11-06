cd55833a8293d70d15a7bf08498e379856257910
Alexandr Rodik <rodik@readdle.com>
2019-11-06 15:55:13 +0200

    fix broken tests

--- test-playground/src/dont-compile.spec.ts
+++
@@ -1,7 +0,0 @@
-describe("don't compile me", () => {
-    test("don't throw type error while compiling project", () => {
-        const x = 1;
-        x = 2;
-        expect(1).toBe(1);
-    });
-});
